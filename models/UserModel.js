d4d19768c05b66b9379cb25c9b9f4982d40b9228
CoderRaushan <raushankumar23082004@gmail.com>
2025-04-01 09:41:19 +0530

    first commit

---
+++ models/UserModel.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    IsVerified: {
+      type: Boolean,
+      default: false,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "others"],
+    },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    providers: [
+      {
+        providerName: {
+          type: String,
+        },
+        providerId: {
+          type: String,
+        },
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
+
+UserSchema.index({ "providers.providerName": 1, "providers.providerId": 1 }, { unique: true });
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
