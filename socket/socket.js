45eb4669292fb570d9e80d19f1fad3ad97750539
CoderRaushan <raushankumar23082004@gmail.com>
2025-04-11 06:54:19 +0530

    message seen status changed

--- socket/socket.js
+++ socket/socket.js
@@ -1,33 +1,64 @@
 import { Server } from "socket.io";
 import express from "express";
 import http from "http";
+import Message from "../models/MessageModel.js";
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-    },
+  cors: {
+    origin: "*",
     methods: ["GET", "POST"],
+  },
 });
 
-const UserSocketMap = {};//isme each user ke crossponding user ka socket id aayega
-export const getReceiverSocketId=(receiverId)=>{
-return UserSocketMap[receiverId];
-}
-io.on("connection",(socket)=>
-{
-    const userId=socket.handshake.query.userId;
-    if(userId)
-    {
-        UserSocketMap[userId]=socket.id;
-        console.log("user connected successfully!",userId,socket.id);
+const UserSocketMap = {};
+
+export const getReceiverSocketId = (receiverId) => {
+  return UserSocketMap[receiverId];
+};
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+
+  if (userId) {
+    UserSocketMap[userId] = socket.id;
+    console.log("User connected:", userId, socket.id);
+  }
+
+  io.emit("OnlineUsers", Object.keys(UserSocketMap));
+
+  socket.on("message-delivered", async (messageId) => {
+    try {
+      await Message.findByIdAndUpdate(messageId, { status: "delivered" });
+      const msg = await Message.findById(messageId);
+      const senderSocket = UserSocketMap[msg.senderId?.toString()];
+      if (senderSocket) {
+        io.to(senderSocket).emit("message-status-updated", msg);
+      }
+    } catch (err) {
+      console.error("message-delivered error:", err.message);
     }
-    io.emit("OnlineUsers",Object.keys(UserSocketMap));
-    socket.on("disconnect",()=>{
-        console.log("user disconnected!",userId);
-        delete UserSocketMap[userId];
-        io.emit("OnlineUsers",Object.keys(UserSocketMap));
-    });
+  });
+
+  socket.on("message-seen", async (messageId) => {
+    try {
+      await Message.findByIdAndUpdate(messageId, { status: "seen" });
+      const msg = await Message.findById(messageId);
+      const senderSocket = UserSocketMap[msg.senderId?.toString()];
+      if (senderSocket) {
+        io.to(senderSocket).emit("message-status-updated", msg);
+      }
+    } catch (err) {
+      console.error("message-seen error:", err.message);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", userId);
+    delete UserSocketMap[userId];
+    io.emit("OnlineUsers", Object.keys(UserSocketMap));
+  });
 });
 
-export {io,app,server};
+export { io, app, server };
